d0541155f9b31702c660fb70980cabb080530d66
jayknyn <hack@jamminglobal.com>
2019-03-29 16:01:44 -0500

    Serving up html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.static('dist'));
 app.listen(3000, () => console.log('JK server running on port 3000'))
-app.get('/', (req, res) => {
-  res.send('Booyah 2 Axe Center')
-})
+// app.get('/', (req, res) => {
+//   res.send('Booyah 2 Axe Center')
+// })
