ae528e8502b0190356d8428ed91d1bfcd4f41a0b
jayknyn <hack@jamminglobal.com>
2019-03-29 15:43:15 -0500

    Checking changes local to remote server

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 app.listen(3000, () => console.log('JK server running on port 3000'))
 app.get('/', (req, res) => {
-  res.send('Booyah 2')
+  res.send('Booyah 2 Axe Center')
 })
