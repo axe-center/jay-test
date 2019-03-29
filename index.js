a481b790bcb197f1f790cc14e461639337fded11
jayknyn <hack@jamminglobal.com>
2019-03-29 15:03:37 -0500

    The server

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+app.listen(3000, () => console.log('JK server running on port 3000'))
+app.get('/', (req, res) => {
+  res.send('Booyah 2')
+})
